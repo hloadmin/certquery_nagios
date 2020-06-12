f4c4f1911a78a4da2c95b85708cdb6096e675fbf
hloadmin <systemsdelivery@aot.com.au>
2020-06-13 07:44:28 +1000

    second certview commit

---
+++ mycredentials.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.8
+from credlib import credential
+#format credentials("login_url","username","password","permissions(boolean)")
+qualys_prod = credential("https://gateway.qgX.apps.qualys.com/auth", "xxxxx", "xxxxxxx","false")
